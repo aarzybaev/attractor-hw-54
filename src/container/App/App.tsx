8b6ff252cb7472c78dafa29cfba6af3b86e36977
Askhad Arzybaev <aarzybaev@gmail.com>
2024-01-08 23:15:38 +0600

    Remove unnecessary tags and rename title

---
+++ src/container/App/App.tsx
@@ -0,0 +1,14 @@
+import GameField from '../../components/GameField/GameField.tsx';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <>
+      <GameField/>
+    </>
+  );
+}
+
+export default App;
